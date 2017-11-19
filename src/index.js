4573c02103792ba66b6f1046c7dcd95570fead80
Shiva Patel <shivapatel15@yahoo.com>
2017-11-19 14:38:04 -0500

    added api key

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = "AIzaSyA_doHPvw6CE3MAQ4ZRkXVwe-yRW-2URmc";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+const App = () => {
+  return (
+    <div>
+      hi
+    </div>
+  );
+};
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ <App />,
+ document.querySelector('.container'));

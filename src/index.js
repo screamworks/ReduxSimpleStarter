ab26390c477d7a92892f1c28fb8cdf7a941e4ef1
Shiva Patel <shivapatel15@yahoo.com>
2017-11-19 23:21:43 -0500

    added search functionality, need to make videotitles appear on the list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list'
 
 const API_KEY = "AIzaSyBlbk6TS7Kd9J4f--p4pSsjvnAfDHu8RC0";
 
+
+
 import SearchBar from './components/search_bar';
 
-const App = () => {
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render(){
   return (
     <div>
       <SearchBar/>
+      <VideoList videos={this.state.videos}/>
     </div>
-  );
+    )
+  }
 };
-ReactDOM.render(
- <App />,
- document.querySelector('.container'));
+
+ReactDOM.render(<App />, document.querySelector('.container'));

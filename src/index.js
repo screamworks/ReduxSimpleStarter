43edcc3004eb536c06d30e7a3a71cbe1d36a6e15
Shiva Patel <shivapatel15@yahoo.com>
2017-11-19 20:37:02 -0500

    added searchBar component functionality

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const API_KEY = "AIzaSyA_doHPvw6CE3MAQ4ZRkXVwe-yRW-2URmc";
+const API_KEY = "AIzaSyBlbk6TS7Kd9J4f--p4pSsjvnAfDHu8RC0";
 
+import SearchBar from './components/search_bar';
 
 const App = () => {
   return (
     <div>
-      hi
+      <SearchBar/>
     </div>
   );
 };
